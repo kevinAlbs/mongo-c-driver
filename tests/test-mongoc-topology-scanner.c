126e88b97690c6ff47dff4d78dbc59ffa8084954
Kevin Albertson <kevin.albertson@10gen.com>
2018-02-26 10:04:47 -0500

    CDRIVER-1972 skip DNS tests on Travis

--- tests/test-mongoc-topology-scanner.c
+++ tests/test-mongoc-topology-scanner.c
@@ -582,6 +582,8 @@ test_topology_scanner_install (TestSuite *suite)
    TestSuite_AddMockServerTest (suite,
                                 "/TOPOLOGY/blocking_initiator",
                                 test_topology_scanner_blocking_initiator);
-   TestSuite_AddMockServerTest (
-      suite, "/TOPOLOGY/dns", test_topology_scanner_dns);
+   if (!test_framework_getenv_bool ("MONGOC_TEST_SKIP_DNS")) {
+      TestSuite_AddMockServerTest (
+         suite, "/TOPOLOGY/dns", test_topology_scanner_dns);
+   }
 }
