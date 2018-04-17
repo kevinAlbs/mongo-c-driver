352a0d44baeaa6fe10c6d0f2580278628037c534
Kevin Albertson <kevin.albertson@10gen.com>
2018-04-17 09:47:22 -0400

    opquery skip

--- tests/test-libmongoc.c
+++ tests/test-libmongoc.c
@@ -2165,7 +2165,7 @@ test_framework_skip_if_max_wire_version_more_than_5 (void)
    if (!TestSuite_CheckLive ()) {
       return 0;
    }
-   return test_framework_max_wire_version_at_least (6);
+   return test_framework_max_wire_version_at_least (6) ? 0 : 1;
 }
 
 int
