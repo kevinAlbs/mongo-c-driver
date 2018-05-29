849601b8c42c8a9bb1ff9f1711a9740c156dedae
Kevin Albertson <kevin.albertson@10gen.com>
2018-05-29 10:10:04 -0400

    fix windows, remove unused mongoc-crypto-cng.h

--- src/libmongoc/src/mongoc/mongoc-crypto-cng-private.h
+++ src/libmongoc/src/mongoc/mongoc-crypto-cng-private.h
@@ -30,19 +30,33 @@
 
 BSON_BEGIN_DECLS
 
-void
+MONGOC_EXPORT (void)
 mongoc_crypto_cng_hmac_sha1 (mongoc_crypto_t *crypto,
                              const void *key,
                              int key_len,
-                             const unsigned char *d,
-                             int n,
-                             unsigned char *md /* OUT */);
+                             const unsigned char *data,
+                             int data_len,
+                             unsigned char *hmac_out);
 
-bool
+MONGOC_EXPORT (bool)
 mongoc_crypto_cng_sha1 (mongoc_crypto_t *crypto,
                         const unsigned char *input,
                         const size_t input_len,
-                        unsigned char *output /* OUT */);
+                        unsigned char *hash_out);
+
+MONGOC_EXPORT (void)
+mongoc_crypto_cng_hmac_sha256 (mongoc_crypto_t *crypto,
+                               const void *key,
+                               int key_len,
+                               const unsigned char *data,
+                               int data_len,
+                               unsigned char *hmac_out);
+
+MONGOC_EXPORT (bool)
+mongoc_crypto_cng_sha256 (mongoc_crypto_t *crypto,
+                          const unsigned char *input,
+                          const size_t input_len,
+                          unsigned char *hash_out);
 
 
 BSON_END_DECLS
