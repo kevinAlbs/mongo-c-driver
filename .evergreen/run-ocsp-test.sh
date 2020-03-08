819ea1c3a16abc0c14752e0fe181c287d806a6a3
Kevin Albertson <kevin.albertson@mongodb.com>
2020-03-08 12:37:45 -0400

    WIP: evergreen

--- .evergreen/run-ocsp-test.sh
+++ .evergreen/run-ocsp-test.sh
@@ -1,38 +1,63 @@
+#! /bin/bash
 # Test runner for OCSP revocation checking.
 #
 # Closely models the tests described in the specification:
 # https://github.com/mongodb/specifications/tree/master/source/ocsp-support/tests#integration-tests-permutations-to-be-tested.
-# Pass the test case as the required first argument, one of the following:
-# "TEST_1", "TEST_2", "TEST_3", "TEST_4", "SOFT_FAIL_TEST", "MALICIOUS_SERVER_TEST_1",
-# "MALICIOUS_SERVER_TEST_2"
-# Ensure mongod is running with the correct configuration.
-# 
-# Example:
-# run-ocsp-test.sh TEST_1
+# Based on the test case, this may start a mock responder process.
+# Precondition: mongod is running with the correct configuration.
 #
-# Optional environment variables:
+# Environment variables:
 #
+# TEST_COLUMN
+#   Required. Corresponds to a column of the test matrix. Set to one of the following:
+#   TEST_1, TEST_2, TEST_3, TEST_4, SOFT_FAIL_TEST, MALICIOUS_SERVER_TEST_1, MALICIOUS_SERVER_TEST_2
+# TEST_ROW
+#   Required. Corresponds to a row of the test matrix. Set to one of the following:
+#   URI_1, URI_2, URI_3
+# CERT_TYPE
+#   Required. Set to either RSA or EDCSA.
+# USE_DELEGATE
+#   Optional. May be ON or OFF. If a test requires use of a responder, this decides whether
+#   the responder uses a delegate certificate. Defaults to "OFF"
 # CDRIVER_BUILD
 #   Optional. The path to the build of mongo-c-driver (e.g. mongo-c-driver/cmake-build).
 #   Defaults to $(pwd)
-# REVOCATION_DISABLED
-#   Optional. Default "OFF". Set to "ON" if the MONGODB_URI includes tlsInsecure or tlsAllowInvalidCertificates.
-# MONGODB_URI
-#   Optional. Defaults to mongodb://localhost:27017/?tls=true.
+# CDRIVER_ROOT
+#   Optional. The path to mongo-c-driver source (may be same as CDRIVER_BUILD).
+#   Defaults to $(pwd)
+# MONGODB_PORT
+#   Optional. A custom port to connect to. Defaults to 27017.
+# SKIP_PIP_INSTALL
+#   Optional. Skip pip install for required packages for mock responder.
+#
+# Example:
+# TEST_COLUMN=TEST_1 TEST_ROW=URI_1 CERT_TYPE=RSA ./run-ocsp-test.sh
 #
 
 # Fail on any command returning a non-zero exit status.
 set -o errexit
 
-TESTCASE=$1
+CDRIVER_ROOT=${CDRIVER_ROOT:-$(pwd)}
 CDRIVER_BUILD=${CDRIVER_BUILD:-$(pwd)}
-MONGODB_URI=${MONGODB_URI-"mongodb://localhost:27017/?tls=true"}
-REVOCATION_DISABLED=${REVOCATION_DISABLED-"OFF"}
+MONGODB_PORT=${MONGODB_PORT-"27017"}
+USE_DELEGATE=${USE_DELEGATE:-OFF}
 
-echo "TESTCASE=$TESTCASE"
+if [ -z "$TEST_COLUMN" -o -z "$TEST_ROW" -o -z "$CERT_TYPE" ]; then
+    echo "Required environment variable unset. See file comments for help."
+    exit 1;
+fi
+echo "TEST_COLUMN=$TEST_COLUMN"
+echo "TEST_ROW=$TEST_ROW"
+echo "CERT_TYPE=$CERT_TYPE"
+echo "USE_DELEGATE=$USE_DELEGATE"
+echo "CDRIVER_ROOT=$CDRIVER_ROOT"
 echo "CDRIVER_BUILD=$CDRIVER_BUILD"
-echo "MONGODB_URI=$MONGODB_URI"
-echo "REVOCATION_DISABLED=$REVOCATION_DISABLED"
+echo "MONGODB_PORT=$MONGODB_PORT"
+echo "SKIP_PIP_INSTALL=$SKIP_PIP_INSTALL"
+
+# Make paths absolute
+CDRIVER_ROOT=$(cd "$CDRIVER_ROOT"; pwd)
+CDRIVER_BUILD=$(cd "$CDRIVER_BUILD"; pwd)
 
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 case "$(uname -s | tr '[:upper:]' '[:lower:]')" in
@@ -40,6 +65,16 @@ case "$(uname -s | tr '[:upper:]' '[:lower:]')" in
     darwin) OS="MACOS" ;;
     *) OS="LINUX" ;;
 esac
+
+on_exit () {
+    echo "Cleaning up"
+    if [ -n "$RESPONDER_REQUIRED" ]; then
+        echo "Responder logs:"
+        cat $CDRIVER_BUILD/responder.logs
+        pkill -f "ocsp_mock" || true
+    fi
+}
+trap on_exit EXIT
 
 MONGOC_PING=$CDRIVER_BUILD/src/libmongoc/mongoc-ping
 
@@ -69,6 +104,45 @@ expect_failure () {
     fi
 }
 
+# Start a mock responder if necessary.
+if curl localhost:8100 > /dev/null 2>&1; then
+    echo "Detected process listening on port 8100. Attempting to kill running mock responders.";
+    pkill -f "ocsp_mock" || true
+fi
+
+# Same responder is used for both server and client. So even stapling tests require a responder.
+if [ "TEST_1" = "$TEST_COLUMN" ]; then
+    RESPONDER_REQUIRED="valid"
+elif [ "TEST_2" = "$TEST_COLUMN" ]; then
+    RESPONDER_REQUIRED="invalid"
+elif [ "TEST_3" = "$TEST_COLUMN" ]; then
+    RESPONDER_REQUIRED="valid"
+elif [ "TEST_4" = "$TEST_COLUMN" ]; then
+    RESPONDER_REQUIRED="invalid"
+elif [ "MALICIOUS_SERVER_TEST_1" = "$TEST_COLUMN" ]; then
+    RESPONDER_REQUIRED="invalid"
+else
+    RESPONDER_REQUIRED=""
+fi
+# oscrypto
+if [ "ON" = "$USE_DELEGATE" ]; then
+    DELEGATE_TOKEN="delegate"
+fi
+
+if [ -n "$RESPONDER_REQUIRED" ]; then
+    echo "Starting mock responder"
+    if [ -z "$SKIP_PIP_INSTALL" ]; then
+        echo "Installing python dependencies"
+        # Installing dependencies.
+        python3 -m venv ./venv
+        . ./venv/bin/activate
+        pip install oscrypto bottle
+    fi
+    cd "$CDRIVER_ROOT/.evergreen/ocsp/$CERT_TYPE"
+    ./mock-$DELEGATE_TOKEN$RESPONDER_REQUIRED.sh > $CDRIVER_BUILD/responder.logs 2>&1 &
+    cd -
+fi
+
 echo "Clearing OCSP cache"
 if [ "$OS" = "MACOS" ]; then
     find ~/profile/Library/Keychains -name 'ocspcache.sqlite3' -exec sqlite3 "{}" 'DELETE FROM responses' \;
@@ -76,21 +150,23 @@ else
     exit 1
 fi
 
+# Construct the MONGODB_URI
+if [ "URI_1" = "$TEST_ROW" ]; then
+    URI_OPTS="tls=true"
+elif [ "URI_2" = "$TEST_ROW" ]; then
+    URI_OPTS="tls=true&tlsInsecure=true"
+elif [ "URI_3" = "$TEST_ROW" ]; then
+    URI_OPTS="tls=true&tlsAllowInvalidCertificates=true"
+fi
+
+MONGODB_URI="mongodb://localhost:$MONGODB_PORT/?$URI_OPTS"
+echo "Connecting with $MONGODB_URI"
+
 # Only a handful of cases are expected to fail.
-if [ "$REVOCATION_DISABLED" = "ON" ]; then
-    expect_success
-    exit 0
-fi
-
-if [ "TEST_1" = "$TESTCASE" ]; then
-    expect_success
-    exit 0
-fi
-
-if [ "TEST_2" = "$TESTCASE" ]; then
+if [ "TEST_2" = "$TEST_COLUMN" -a "URI_1" = "$TEST_ROW" ]; then
     expect_failure
     exit 0
 fi
 
-echo "Unexpected testcase '$TESTCASE'"
-exit 1
+expect_success
+exit 0
