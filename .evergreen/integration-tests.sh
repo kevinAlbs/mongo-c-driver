30c66e64b2ac1d6d87d91ca764815972d36b7bf6
Kevin Albertson <kevin.albertson@mongodb.com>
2020-03-10 15:57:31 -0400

    more

--- .evergreen/integration-tests.sh
+++ .evergreen/integration-tests.sh
@@ -60,22 +60,23 @@ if [ ! -z "$AUTHSOURCE" ]; then
       ORCHESTRATION_FILE="${ORCHESTRATION_FILE}-${AUTHSOURCE}"
    fi
    MONGO_SHELL_CONNECTION_FLAGS="${MONGO_SHELL_CONNECTION_FLAGS} --authenticationDatabase ${AUTHSOURCE}"
-fi
+fi   
 
-if [ "$SSL" != "nossl" ]; then
+if [ "$OCSP" != "off" ];
+   # Replace ABSOLUTE_PATH_REPLACEMENT_TOKEN with path to mongo-c-driver.
+   FULL_PATH=$(pwd)
+   find orchestration_configs -name \*.json | xargs perl -p -i -e "s|ABSOLUTE_PATH_REPLACEMENT_TOKEN|$FULL_PATH|g"
+   MONGO_SHELL_CONNECTION_FLAGS="$MONGO_SHELL_CONNECTION_FLAGS --host localhost --tls --tlsAllowInvalidCertificates"
+elif [ "$SSL" != "nossl" ]; then
    cp -f src/libmongoc/tests/x509gen/* $MONGO_ORCHESTRATION_HOME/lib/
    # find print0 and xargs -0 not available on Solaris. Lets hope for good paths
    find orchestration_configs -name \*.json | xargs perl -p -i -e "s|/tmp/orchestration-home|$MONGO_ORCHESTRATION_HOME/lib|g"
    if [ -z "$ORCHESTRATION_FILE_PASSED" ]; then
       ORCHESTRATION_FILE="${ORCHESTRATION_FILE}-ssl"
    fi
-fi
-
-if [ "$OCSP" != "off" ]; then
-   # Replace ABSOLUTE_PATH_REPLACEMENT_TOKEN with path to mongo-c-driver.
-   FULL_PATH=$(pwd)
-   find orchestration_configs -name \*.json | xargs perl -p -i -e "s|ABSOLUTE_PATH_REPLACEMENT_TOKEN|$FULL_PATH|g"
-   MONGO_SHELL_CONNECTION_FLAGS="--host localhost --tls --tlsAllowInvalidCertificates"
+   MONGO_SHELL_CONNECTION_FLAGS="$MONGO_SHELL_CONNECTION_FLAGS --host localhost --ssl --sslCAFile=$MONGO_ORCHESTRATION_HOME/lib/ca.pem --sslPEMKeyFile=$MONGO_ORCHESTRATION_HOME/lib/client.pem"
+else
+   
 fi
 
 export ORCHESTRATION_FILE="orchestration_configs/${TOPOLOGY}s/${ORCHESTRATION_FILE}.json"
@@ -104,9 +105,6 @@ case "$OS" in
       cd ../..
       ls `pwd`/mongodb/bin/mongo* || true
       nohup mongo-orchestration -f orchestration.config -e default --socket-timeout-ms=60000 --bind=127.0.0.1  --enable-majority-read-concern -s wsgiref start > $MONGO_ORCHESTRATION_HOME/out.log 2> $MONGO_ORCHESTRATION_HOME/err.log < /dev/null &
-      if [ "$SSL" != "nossl" ]; then
-         export MONGO_SHELL_CONNECTION_FLAGS="$MONGO_SHELL_CONNECTION_FLAGS --host localhost --ssl --sslCAFile=$MONGO_ORCHESTRATION_HOME/lib/ca.pem --sslPEMKeyFile=$MONGO_ORCHESTRATION_HOME/lib/client.pem"
-      fi
       ;;
    *)
       echo "{ \"releases\": { \"default\": \"`pwd`/mongodb/bin\" } }" > orchestration.config
@@ -132,9 +130,6 @@ case "$OS" in
       pip $PIP_PARAM install .
       cd ../..
       mongo-orchestration -f orchestration.config -e default --socket-timeout-ms=60000 --bind=127.0.0.1  --enable-majority-read-concern start > $MONGO_ORCHESTRATION_HOME/out.log | tee 2> $MONGO_ORCHESTRATION_HOME/err.log < /dev/null &
-      if [ "$SSL" != "nossl" ]; then
-         export MONGO_SHELL_CONNECTION_FLAGS="$MONGO_SHELL_CONNECTION_FLAGS --host localhost --ssl --sslCAFile=$MONGO_ORCHESTRATION_HOME/lib/ca.pem --sslPEMKeyFile=$MONGO_ORCHESTRATION_HOME/lib/client.pem"
-      fi
       ;;
 esac
 
