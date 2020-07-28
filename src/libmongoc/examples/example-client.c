a280f9cce981ccb67dbcd5239b464ae51a045b21
Kevin Albertson <kevin.albertson@mongodb.com>
2020-07-27 22:51:29 -0400

    add multi-threaded example

--- src/libmongoc/examples/example-client.c
+++ src/libmongoc/examples/example-client.c
@@ -1,77 +1,71 @@
-/* gcc example-client.c -o example-client $(pkg-config --cflags --libs
- * libmongoc-1.0) */
-
-/* ./example-client [CONNECTION_STRING [COLLECTION_NAME]] */
-
 #include <mongoc/mongoc.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+#define TRIALS 10
+
+void * worker (void* pool_void) {
+   mongoc_client_pool_t *pool;
+   mongoc_client_t *client;
+   bson_t ping;
+   bson_error_t error;
+   int i;
+
+   bson_init (&ping);
+   BCON_APPEND (&ping, "ping", BCON_INT32 (1));
+   pool = (mongoc_client_pool_t *) pool_void;
+
+   for (i = 0; i < TRIALS; i++) {
+      client = mongoc_client_pool_pop (pool);
+      if (!mongoc_client_command_simple (client, "db", &ping, NULL, NULL, &error)) {
+         fprintf (stderr, "ping error: %s", error.message);
+         exit (1);
+      }
+      mongoc_client_pool_push (pool, client);
+   }
+
+   bson_destroy (&ping);
+   return NULL;
+}
 
 int
 main (int argc, char *argv[])
 {
-   mongoc_client_t *client;
-   mongoc_collection_t *collection;
-   mongoc_cursor_t *cursor;
-   bson_error_t error;
-   const bson_t *doc;
-   const char *collection_name = "test";
-   bson_t query;
-   char *str;
-   const char *uri_string = "mongodb://127.0.0.1/?appname=client-example";
+   int nthreads;
+   int i;
+   pthread_t *threads;
+   char *uri_str;
    mongoc_uri_t *uri;
+   mongoc_client_pool_t *pool;
+   bson_error_t error;
 
    mongoc_init ();
-   if (argc > 1) {
-      uri_string = argv[1];
+
+   if (argc != 3) {
+      fprintf (stderr, "./example-client <uri> <# threads>\n");
+      return EXIT_FAILURE;
    }
 
-   if (argc > 2) {
-      collection_name = argv[2];
-   }
-
-   uri = mongoc_uri_new_with_error (uri_string, &error);
+   uri_str = argv[1];
+   nthreads = atoi (argv[2]);
+   uri = mongoc_uri_new_with_error (uri_str, &error);
    if (!uri) {
-      fprintf (stderr,
-               "failed to parse URI: %s\n"
-               "error message:       %s\n",
-               uri_string,
-               error.message);
+      fprintf (stderr, "uri error: %s", error.message);
       return EXIT_FAILURE;
    }
 
-   client = mongoc_client_new_from_uri (uri);
-   if (!client) {
-      return EXIT_FAILURE;
+   pool = mongoc_client_pool_new (uri);
+   threads = (pthread_t *) bson_malloc (sizeof (pthread_t) * nthreads);
+   for (i = 0; i < nthreads; i++) {
+      pthread_create (threads + i, NULL /* attr */, worker, pool);
    }
 
-   mongoc_client_set_error_api (client, 2);
-
-   bson_init (&query);
-   collection = mongoc_client_get_collection (client, "test", collection_name);
-   cursor = mongoc_collection_find_with_opts (
-      collection,
-      &query,
-      NULL,  /* additional options */
-      NULL); /* read prefs, NULL for default */
-
-   while (mongoc_cursor_next (cursor, &doc)) {
-      str = bson_as_canonical_extended_json (doc, NULL);
-      fprintf (stdout, "%s\n", str);
-      bson_free (str);
+   for (i = 0; i < nthreads; i++) {
+      pthread_join (threads[i], NULL);
    }
 
-   if (mongoc_cursor_error (cursor, &error)) {
-      fprintf (stderr, "Cursor Failure: %s\n", error.message);
-      return EXIT_FAILURE;
-   }
-
-   bson_destroy (&query);
-   mongoc_cursor_destroy (cursor);
-   mongoc_collection_destroy (collection);
+   mongoc_client_pool_destroy (pool);
    mongoc_uri_destroy (uri);
-   mongoc_client_destroy (client);
    mongoc_cleanup ();
-
-   return EXIT_SUCCESS;
 }
