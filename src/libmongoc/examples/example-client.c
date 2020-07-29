237832cd374f7a7f17838cb78d2073e37621b16b
Kevin Albertson <kevin.albertson@mongodb.com>
2020-07-29 11:06:12 -0400

    vary commands + read prefs

--- src/libmongoc/examples/example-client.c
+++ src/libmongoc/examples/example-client.c
@@ -5,27 +5,41 @@
 
 #define TRIALS 10
 
-void * worker (void* pool_void) {
+void *
+worker (void *pool_void)
+{
    mongoc_client_pool_t *pool;
    mongoc_client_t *client;
    bson_t ping;
+   bson_t server_status;
    bson_error_t error;
    int i;
+   mongoc_read_prefs_t *secondary_prefs;
 
    bson_init (&ping);
    BCON_APPEND (&ping, "ping", BCON_INT32 (1));
    pool = (mongoc_client_pool_t *) pool_void;
+   secondary_prefs = mongoc_read_prefs_new (MONGOC_READ_SECONDARY);
+   bson_init (&server_status);
+   BCON_APPEND (&server_status, "serverStatus", BCON_INT32 (1));
 
    for (i = 0; i < TRIALS; i++) {
       client = mongoc_client_pool_pop (pool);
-      if (!mongoc_client_command_simple (client, "db", &ping, NULL, NULL, &error)) {
+      if (!mongoc_client_command_simple (client,
+                                         "db",
+                                         i % 2 == 0 ? &ping : &server_status,
+                                         i % 2 == 0 ? secondary_prefs : NULL,
+                                         NULL,
+                                         &error)) {
          fprintf (stderr, "ping error: %s", error.message);
          exit (1);
       }
       mongoc_client_pool_push (pool, client);
    }
 
+   bson_destroy (&server_status);
    bson_destroy (&ping);
+   mongoc_read_prefs_destroy (secondary_prefs);
    return NULL;
 }
 
@@ -55,6 +69,10 @@ main (int argc, char *argv[])
       return EXIT_FAILURE;
    }
 
+   MONGOC_DEBUG ("spawning %d threads, and running %d operations per thread",
+                 nthreads,
+                 TRIALS);
+
    pool = mongoc_client_pool_new (uri);
    threads = (pthread_t *) bson_malloc (sizeof (pthread_t) * nthreads);
    for (i = 0; i < nthreads; i++) {
@@ -64,6 +82,8 @@ main (int argc, char *argv[])
    for (i = 0; i < nthreads; i++) {
       pthread_join (threads[i], NULL);
    }
+
+   MONGOC_DEBUG ("done");
 
    mongoc_client_pool_destroy (pool);
    mongoc_uri_destroy (uri);
