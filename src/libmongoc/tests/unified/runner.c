6c9f5e0dfabb14344e1c7ed15f4e3d9f58a15f62
Kevin Albertson <kevin.albertson@mongodb.com>
2021-06-04 11:50:16 -0400

    CDRIVER-3897 add tests for time-series collections (#799)

--- src/libmongoc/tests/unified/runner.c
+++ src/libmongoc/tests/unified/runner.c
@@ -1234,7 +1234,7 @@ test_check_outcome_collection (test_t *test,
       expected_sorted = bson_copy_and_sort (&expected);
 
 
-      if (!bson_equal (&actual, &expected)) {
+      if (!bson_equal (actual_sorted, expected_sorted)) {
          test_set_error (error,
                          "expected %s, but got %s",
                          tmp_json (expected_sorted),
@@ -1589,4 +1589,6 @@ test_install_unified (TestSuite *suite)
    run_unified_tests (suite, JSON_DIR "/crud/unified");
 
    run_unified_tests (suite, JSON_DIR "/transactions/unified");
+
+   run_unified_tests (suite, JSON_DIR "/collection-management");
 }
