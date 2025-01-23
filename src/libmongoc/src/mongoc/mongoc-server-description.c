c6372bdfd472914b7eebd5000e99d26e407d1f68
Kevin Albertson <kevin.albertson@mongodb.com>
2025-01-22 19:39:49 -0500

    add callcount

--- src/libmongoc/src/mongoc/mongoc-server-description.c
+++ src/libmongoc/src/mongoc/mongoc-server-description.c
@@ -759,6 +759,13 @@ authfailure:
    EXIT;
 }
 
+static size_t mongoc_server_description_new_copy_callcount = 0;
+
+MONGOC_EXPORT (size_t)
+get_mongoc_server_description_new_copy_callcount (void)
+{
+   return mongoc_server_description_new_copy_callcount;
+}
 /*
  *-------------------------------------------------------------------------
  *
@@ -813,6 +820,8 @@ mongoc_server_description_new_copy (const mongoc_server_description_t *descripti
 
    copy->generation = description->generation;
    copy->_generation_map_ = mongoc_generation_map_copy (mc_tpl_sd_generation_map_const (description));
+
+   mongoc_server_description_new_copy_callcount++;
    return copy;
 }
 
